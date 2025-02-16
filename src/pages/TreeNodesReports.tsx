d24368868f8461349fd0787ee931f93758d637d5
omri <omrigamer100@gmail.com>
2025-02-17 00:56:03 +0200

    test

--- src/pages/TreeNodesReports.tsx
+++ src/pages/TreeNodesReports.tsx
@@ -1,11 +1,8 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { fetch_root_nodes } from "../services/Get-Root-Nodes";
+import { fetch_nodes_report } from "../services/Get-Nodes-Reports";
 import { useAuthContext } from "../Context/UseAuthContext";
-import { Box } from "@mui/material";
-import { Link } from "react-router-dom";
-import { Typography } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { Typography, Box } from "@mui/material";
 
 type sub_nodes = {
   description: string;
@@ -35,12 +32,34 @@ type data = {
 export const TreeNodesReports = () => {
   const [nodesData, setNodesData] = useState<data | null>(null);
   const { id } = useParams();
-  const navigate = useNavigate();
+  const { user } = useAuthContext();
 
-  console.log(id);
+  const get_reports_nodes = async () => {
+    const response = await fetch_nodes_report(user.token, id);
+    setNodesData(response.data);
+  };
+
+  useEffect(() => {
+    get_reports_nodes();
+  }, []);
+
   return (
-    <div>
-      <h1>{id}</h1>
-    </div>
+    <Box>
+      {nodesData?.reports && nodesData.reports.length > 0 && (
+        <Typography variant="h6" color="primary">
+          Reports found
+        </Typography>
+      )}
+      {nodesData?.nodes && nodesData.nodes.length > 0 && (
+        <Typography variant="h6" color="secondary">
+          Nodes found
+        </Typography>
+      )}
+      {nodesData?.nodes?.length === 0 && nodesData?.reports?.length === 0 && (
+        <Typography variant="h6" color="secondary">
+          Nothing found
+        </Typography>
+      )}
+    </Box>
   );
 };
---
+++ src/services/Get-Nodes-Reports.ts
@@ -0,0 +1,15 @@
+import api from "./Http";
+
+export const fetch_nodes_report = async (token: string, id: string | undefined) => {
+  try {
+    const response = await api.get(`/api/v1/reports/nodes/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
