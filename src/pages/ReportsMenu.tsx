b75f9d8200d0a5cd4bfd954a27675be304d3f49c
omri <omrigamer100@gmail.com>
2025-02-17 15:57:06 +0200

    test

--- src/pages/ReportsMenu.tsx
+++ src/pages/ReportsMenu.tsx
@@ -6,27 +6,28 @@ import type { FieldValues } from "react-hook-form";
 import formsvg from "../assets/form.svg";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
 import { Bounce } from "react-toastify";
 import api from "../services/Http";
 import { useAuthContext } from "../Context/UseAuthContext";
 import { useParams } from "react-router-dom";
 
+interface report {
+  report_id: string;
+  title: string;
+  description: string;
+}
+
 export const ReportsMenu = () => {
-    const {user} = useAuthContext()
-    const {id} = useParams()
+  const { user } = useAuthContext();
 
+  const [reports, setReports] = useState<report[]>([]);
+  const { id } = useParams();
 
-
-    
-
-    
   return (
     <div>
-        <h1>
-            {id}
-        </h1>
+      <h1>{id}</h1>
     </div>
-  )
-}
+  );
+};
