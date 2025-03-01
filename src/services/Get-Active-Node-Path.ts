f3be45c1659ea4f5309da7e277b233b98cc6b77a
omri <omrigamer100@gmail.com>
2025-03-01 06:48:33 +0200

    test

---
+++ src/services/Get-Active-Node-Path.ts
@@ -0,0 +1,11 @@
+import api from "./Http";
+
+export const get_active_node_path = async (token: string, node_id: any) => {
+  const response = await api.get(`/api/v1/reports/nodes/path/${node_id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response;
+};
