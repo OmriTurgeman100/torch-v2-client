a99c13c3cd6bb06ceafdb40eb476810854dcf5fc
omri <omrigamer100@gmail.com>
2025-03-01 04:06:51 +0200

    test

--- src/Context/BreadcrumbContext.tsx
+++ src/Context/BreadcrumbContext.tsx
@@ -1,17 +1,11 @@
-import {
-  createContext,
-  useReducer,
-  useState,
-  useEffect,
-  useContext,
-} from "react";
+import { createContext, useState } from "react";
 
 export const BreadCrumbContext = createContext<any>(null);
 
 export const BreadcrumbProvider = ({ children }: any) => {
   const [BreadCrumbPath, setBreadCrumbPath] = useState<string>("");
   return (
-    <BreadCrumbContext.Provider value={BreadCrumbPath}>
+    <BreadCrumbContext.Provider value={{ BreadCrumbPath, setBreadCrumbPath }}>
       {children}
     </BreadCrumbContext.Provider>
   );
