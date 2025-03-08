152ce7a4657cc4edfb239b927b94a92fbebf394d
omri <omrigamer100@gmail.com>
2025-03-08 13:17:03 +0200

    test

--- src/Context/ThemeContext.tsx
+++ src/Context/ThemeContext.tsx
@@ -5,6 +5,14 @@ export const ThemeContext = createContext<any>(null);
 export const ThemeContextProvider = ({ children }: { children: any }) => {
   const [Theme, setTheme] = useState<string | null>("light");
 
+  useEffect(() => {
+    if (Theme === "light") {
+      document.body.style.backgroundColor = "#e9ecef"; // light theme color
+    } else if (Theme === "dark") {
+      document.body.style.backgroundColor = "#121212"; // dark theme color
+    }
+  }, [Theme]);
+
   return (
     <ThemeContext.Provider value={{ Theme, setTheme }}>
       {children}
