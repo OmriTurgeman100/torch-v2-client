4df4dba34b9f35ab1ddebdf230ddc72cc997a369
omri <omrigamer100@gmail.com>
2025-03-08 13:11:07 +0200

    test

--- src/Context/ThemeContext.tsx
+++ src/Context/ThemeContext.tsx
@@ -3,7 +3,7 @@ import { createContext, useState, useEffect } from "react";
 export const ThemeContext = createContext<any>(null);
 
 export const ThemeContextProvider = ({ children }: { children: any }) => {
-  const [Theme, setTheme] = useState<string | null>(null);
+  const [Theme, setTheme] = useState<string | null>("light");
 
   return (
     <ThemeContext.Provider value={{ Theme, setTheme }}>
