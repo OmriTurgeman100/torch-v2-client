e76977531fbf47762851b5d0b3160b5b976e293c
omri <omrigamer100@gmail.com>
2025-03-08 13:04:19 +0200

    test

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { AuthContextProvider } from "./Context/AuthContext.tsx";
+import { ThemeContextProvider } from "./Context/ThemeContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthContextProvider>
-      <App />
+      <ThemeContextProvider>
+        <App />
+      </ThemeContextProvider>
     </AuthContextProvider>
   </StrictMode>
 );
