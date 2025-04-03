12ad8301f0b7e3700f25e227516f5706d2e27b10
omri <omrigamer100@gmail.com>
2025-04-03 14:10:35 +0300

    removed token

--- src/services/Get-Root-Nodes.ts
+++ src/services/Get-Root-Nodes.ts
@@ -9,3 +9,13 @@ export const fetch_root_nodes = async (token: string) => {
 
   return response.data;
 };
+
+// import api from "./Http";
+
+// export const fetch_root_nodes = async (token: string) => {
+//   console.log("Token:", token); // Just log the token
+
+//   const response = await api.get("/api/v1/reports/nodes");
+
+//   return response.data;
+// };
--- src/services/Http.ts
+++ src/services/Http.ts
@@ -4,3 +4,21 @@ export default axios.create({
   baseURL: import.meta.env.VITE_BACKEND || "http://localhost:3000",
 });
 // * set env before up being built.
+
+// import axios from "axios";
+
+// // Create instance
+// // const api = axios.create({
+// //   baseURL: import.meta.env.VITE_BACKEND || "http://localhost:3000",
+// // });
+
+// // // Interceptor to attach token to every request
+// // api.interceptors.request.use((config) => {
+// //   const token = localStorage.getItem("token"); // or use a cookie, or context, depending on your setup
+// //   if (token) {
+// //     config.headers.Authorization = `Bearer ${token}`;
+// //   }
+// //   return config;
+// // });
+
+// // export default api;
